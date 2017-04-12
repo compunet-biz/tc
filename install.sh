13dd8712ec088aaf91eb88ecd8cb3dd25b710dcb
Josh Guice <jguice@gmail.com>
2017-04-12 12:03:24 -0700

    fix(install): cwd wasn't where it seemed to be

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ LOG_FILE=/tmp/tc-install.log
 echo "---------------------------------------" >> $LOG_FILE
 date >> $LOG_FILE
 [[ -d $INSTALL_DIR ]] || mkdir $INSTALL_DIR >> $LOG_FILE
-rsync -av --delete --force --exclude="storage/*/**" --exclude="vendor/" --exclude=".git/" --exclude=".gitignore" --exclude=".env" --filter "protect .env" --filter "protect down" --filter "protect vendor/" --filter "protect storage/*/**" . /opt/bitnami/apps/tc/ >> $LOG_FILE 2>&1
+rsync -av --delete --force --exclude="storage/*/**" --exclude="vendor/" --exclude=".git/" --exclude=".gitignore" --exclude=".env" --filter "protect .env" --filter "protect down" --filter "protect vendor/" --filter "protect storage/*/**" /opt/remoteFiles/appPackage/tc-master/ /opt/bitnami/apps/tc/ >> $LOG_FILE 2>&1
 (cd $INSTALL_DIR && HOME=/home/cliqruser /opt/bitnami/php/bin/composer install >> $LOG_FILE 2>&1)
 
