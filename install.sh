1f4c59f1e8a9ce02690a161856eb274f952f8364
Josh Guice <jguice@gmail.com>
2017-04-12 11:57:31 -0700

    feat(install): use rsync + logging

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 INSTALL_DIR=/opt/bitnami/apps/tc
+LOG_FILE=/tmp/tc-install.log
 
-[[ -d $INSTALL_DIR ]] || mkdir $INSTALL_DIR
-cp -R * $INSTALL_DIR
-(cd $INSTALL_DIR && HOME=/home/cliqruser /opt/bitnami/php/bin/composer install)
+echo "---------------------------------------" >> $LOG_FILE
+date >> $LOG_FILE
+[[ -d $INSTALL_DIR ]] || mkdir $INSTALL_DIR >> $LOG_FILE
+rsync -av --delete --force --exclude="storage/*/**" --exclude="vendor/" --exclude=".git/" --exclude=".gitignore" --exclude=".env" --filter "protect .env" --filter "protect down" --filter "protect vendor/" --filter "protect storage/*/**" . /opt/bitnami/apps/tc/ >> $LOG_FILE 2>&1
+(cd $INSTALL_DIR && HOME=/home/cliqruser /opt/bitnami/php/bin/composer install >> $LOG_FILE 2>&1)
 
